2962ec3ab732052228ef39de277a98dc02fe90bb
loivu2001 <107788859+loivu2001@users.noreply.github.com>
2023-10-11 09:49:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/godfd/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.godfd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        var diceImage: ImageView = findViewById(R.id.imageView)
+        var diceRoll = (1..6).random()
+        when(diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+
+        }
+
+    }
+
+}
